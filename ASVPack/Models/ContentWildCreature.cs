cbe1a1c5cf2a63e785d4b53e950736911ebe00e5
Ian Ashworth <cyber_mirage@hotmail.com>
2022-12-26 20:08:20 +0000

    Added checks for alpha creatures to exclude from IsTameable.

--- ASVPack/Models/ContentWildCreature.cs
+++ ASVPack/Models/ContentWildCreature.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -22,8 +23,8 @@ namespace ASVPack.Models
         public ContentWildCreature(GameObject gameObject, GameObject statusObject) : base(gameObject, statusObject)
         {
             IsTameable = !gameObject.GetPropertyValue<bool>("bForceDisablingTaming");
-
-     
+            var isAlpha = Regex.IsMatch(ClassName, "Mega[A-Z]") || ClassName.Contains("Mega_") || ClassName.Contains("Alpha_");
+            if (isAlpha) IsTameable = false;
         }
     }
 }
